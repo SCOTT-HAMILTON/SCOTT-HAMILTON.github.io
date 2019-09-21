28b0f41b0ab146945b576d1235024cc46014b5d0
SCOTT-HAMILTON <sgn.hamilton@protonmail.com>
2019-09-21 20:11:36 +0200

    Images System, css for pages, SVT images

--- Work/generate_subject_webpages.py
+++ Work/generate_subject_webpages.py
@@ -7,7 +7,31 @@ import yaml
 import threading
 from pathlib import Path
 
-def generate_content(matiere, pdfs, file_path, configs=None):
+def gen_files_list(files, prefix=""):
+    files_link_content=""
+    template_link_content= \
+"""\t<a href=\"<url>\"><name></a>
+\t<p>(<description>)</p>"""
+    for file in files:
+        print(" file : ", file)
+        config_file = file[:-4]+'.yaml'
+        print("config file yaml : ",config_file)
+        config = load_config(config_file)
+        name = "Unknown file"
+        desc = "Empty description"
+
+        if config:
+            name = config["name"]
+            desc = config["description"]
+
+        buff = template_link_content
+        buff = re.sub('<url>', prefix+file.split('/')[-1], buff)
+        buff = re.sub('<name>', name, buff)
+        buff = re.sub('<description>', desc, buff)
+        files_link_content += buff +'\n'
+    return files_link_content 
+
+def generate_content(matiere, pdfs, images, file_path, configs=None):
     content = Path("sample_subject_webpage.html").read_text() 
 
     auth = "The author"
@@ -22,29 +46,11 @@ def generate_content(matiere, pdfs, file_path, configs=None):
     content = re.sub('<theauthor>',auth, content)
     content = re.sub('<thetitle>',title, content)
     
-    files_link_content=""
-    template_link_content= \
-"""\t<a href=\"<url>\"><name></a>
-\t<p>(<description>)</p>"""
-    for pdf in pdfs:
-        print(matiere, "pdf file : ", pdf)
-        config_file = pdf[:-4]+'.yaml'
-        print("pdf config file yaml : ",config_file)
-        config_pdf = load_config(config_file)
-        name = "Unknown file"
-        desc = "Empty description"
+    files_link_content = gen_files_list(pdfs)
+    images_link_content = gen_files_list(images, "images/")
 
-        if config_pdf:
-            name = config_pdf["name"]
-            desc = config_pdf["description"]
-
-        buff = template_link_content
-        buff = re.sub('<url>', pdf.split('/')[-1], buff)
-        buff = re.sub('<name>', name, buff)
-        buff = re.sub('<description>', desc, buff)
-        files_link_content += buff +'\n' 
-    
     content = re.sub('<thefiles>', files_link_content, content)
+    content = re.sub('<theimages>', images_link_content, content)
 
     file = open(file_path, "w+")
     file.write(content)
@@ -66,6 +72,16 @@ def list_pdfs(path):
 
     return files
 
+def list_images(path):
+    files = []
+    try:
+        for file in os.listdir(path):
+            if file.endswith(".jpg") or file.endswith(".jpeg"):
+                files.append(os.path.join(path, file))
+    except:
+        print("No images")
+    return files
+
 threads = []
 
 for line in sys.stdin:
@@ -73,7 +89,8 @@ for line in sys.stdin:
     print("args : ", mat)
     file_path = "./"+mat+"/index.html"
     pdfs = list_pdfs("./"+mat)
-    thread = threading.Thread(target=generate_content, args=(mat, pdfs, file_path, load_config("./"+mat+"/info.yaml")))
+    images = list_images("./"+mat+"/images")
+    thread = threading.Thread(target=generate_content, args=(mat, pdfs, images, file_path, load_config("./"+mat+"/info.yaml")))
     thread.start()
     threads.append(thread)
 
