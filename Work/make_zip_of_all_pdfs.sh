5b7dcf5a7367ecd5e8a8bfbf4c36c95015b21f15
SCOTT-HAMILTON <sgn.hamilton@protonmail.com>
2019-09-23 08:17:23 +0200

    all files zip bub fixed

--- Work/make_zip_of_all_pdfs.sh
+++ Work/make_zip_of_all_pdfs.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-find . -name "*.pdf" -exec cp {} /tmp/pdfs \; && cdir=$PWD && cd /tmp && zip -r all_pdfs.zip pdfs && cd $cdir && mv /tmp/all_pdfs.zip .
+rm -rf /tmp/pdfs
+mkdir /tmp/pdfs
+find . -name "*.pdf" -exec cp {} /tmp/pdfs \;
+find . -regextype sed -regex ".*\/images.*[\.jpg|\.jpeg]" -exec cp {} /tmp/pdfs \;
+
+cdir=$PWD
+cd /tmp
+zip -r all_pdfs.zip pdfs
+cd $cdir
+mv /tmp/all_pdfs.zip .
