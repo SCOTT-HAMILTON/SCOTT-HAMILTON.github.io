ee1e2dc249906e04f7cf0a0b5468935675103651
SCOTT-HAMILTON <sgn.hamilton@protonmail.com>
2019-09-29 19:33:02 +0200

    file accent cleaning

--- Work/clean_names.sh
+++ Work/clean_names.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+find $(pwd) -name "*"| awk '{print $0" "$0}' | awk '{str=$2;gsub(/é/,"e",str);print $1" "str}' | xargs -r -L1 "mv"  >/dev/null 2>&1
 find . -name "*.md*.pdf" | awk '{printf("%s ",$0);str = $0;sub(/\.md/, "", str);print str}' | xargs -r -L1 mv
 find . -name "*.md*.yaml" | awk '{printf("%s ",$0);str = $0;sub(/\.md/, "", str);print str}' | xargs -r -L1 mv
