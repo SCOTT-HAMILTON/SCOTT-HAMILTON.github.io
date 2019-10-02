0b88c57433144da5265e69ff2d0824546be50d89
SCOTT-HAMILTON <sgn.hamilton@protonmail.com>
2019-10-02 20:26:48 +0200

    Zip generator update : separation of files by directories

--- Work/make_zip_of_all_pdfs.sh
+++ Work/make_zip_of_all_pdfs.sh
@@ -2,11 +2,15 @@
 
 rm -rf /tmp/pdfs
 mkdir /tmp/pdfs
-find . -name "*.pdf" -exec cp {} /tmp/pdfs \;
-find . -regextype sed -regex ".*\/images.*[\.jpg|\.jpeg]" -exec cp {} /tmp/pdfs \;
+find . -name "*.pdf" | awk -F / -f filter_subjects_no_duplicate.awk | awk '{print "/tmp/pdfs/"$0}' | xargs -r -L1 mkdir
+find . -name "*.pdf" | awk -F'/' '{print $0" /tmp/pdfs/"$2}' | xargs -r -L1 cp
+find . -regextype sed -regex ".*\/images.*[\.jpg|\.jpeg]" | awk -F'/' '{print $0" /tmp/pdfs/"$2}' | xargs -r -L1 cp 
 
 cdir=$PWD
 cd /tmp
 zip -r all_pdfs.zip pdfs
 cd $cdir
 mv /tmp/all_pdfs.zip .
+
+rm -rf /tmp/pdfs
+
